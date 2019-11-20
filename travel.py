87118e5450c7cda512f0910d0f5272d9942240bf
MdAsif25 <36000940+MdAsif25@users.noreply.github.com>
2019-11-20 03:35:57 +0000

    Add files via upload

---
+++ travel.py
@@ -0,0 +1,34 @@
+name=input("enter your name:")
+age=input("enter your age:")
+phone=input("enter your phone number:")
+mail=input("enter your mail:")
+source=input("source:")
+destination=input("destination:")
+if(source==destination):
+    print("don't select the same location")
+if(source=="salem"):
+    source=int(0)
+if(source=="pachal"):
+    source=int(25)
+if(source=="namkkal"):
+    source=int(50)
+if(destination=="salem"):
+    destination=int(0)
+if(destination=="pachal"):
+    destination=int(25)
+if(destination=="namkkal"):
+    destination=int(50)
+dist=destination-source
+print(dist)
+vehicles=["car","bus"]
+print(vehicles)
+veh=input("vehicles:")
+customer=["premium","ordinary"]
+print(customer)
+cust=input("cutomertype:")
+if(cust=="premium"):
+    amt=7*dist
+    print("amount is ",amt)
+else:
+    amt=12*dist
+    print("amount is",amt)
