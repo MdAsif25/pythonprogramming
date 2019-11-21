05fd8ea5a78b4654623fd63aab7b8208c28e8813
MdAsif <36000940+MdAsif25@users.noreply.github.com>
2019-11-21 10:58:41 +0530

    Create oddevennum.py

---
+++ oddevennum.py
@@ -0,0 +1,15 @@
+m=int(input("enter your source range"))
+n=int(input("enter your destination range"))
+for i in range(m,n):
+  if(i%2==0):
+    print("even number is :",i)
+  elif(i!=0):
+     print("odd number is:",i)
+  
+
+  
+
+ 
+  
+
+ 
